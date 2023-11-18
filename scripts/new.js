59a680061a0194c55552e4cfda5a2c931a47f492
chrisdecara <cdecara@ufl.edu>
2023-11-17 19:07:22 -0500

    potential final commit

--- scripts/new.js
+++ scripts/new.js
@@ -4,3 +4,9 @@ L.tileLayer('https://api.mapbox.com/styles/v1/chrisdecara/clp33v8a300zi01ns93c1a
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
 }).addTo(map);
+
+for ( let i = 0; i < places.length; i++ ) {
+    L.marker( [ places[i].latitude, places[i].longitude ] )
+.bindPopup( '<h3>' + places[i].placeName + '</h3>' + '<p>' + places[i].descrip + '</p>')
+.addTo( map );
+}
